19f1f107f82fc8cbd2af751af43760b7cb66bd59
DemoSystem <nheoumveasna12@gmail.com>
2024-06-06 17:14:50 +0700

    first c programming

---
+++ cmake-build-debug/Information_details.h
@@ -0,0 +1,8 @@
+//
+// Created by Veasna on 5/30/2024.
+//
+
+#ifndef INFORMATION_DETAILS_H
+#define INFORMATION_DETAILS_H
+
+#endif //INFORMATION_DETAILS_H
---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int main(void) {
+   // char FullName [] = "NHEOUM VEASNA";
+   // int age = 20;
+   // char gender = "Male";
+   // char province[] = "Siem Reap";
+   // char major[] = "Digital Marketing";
+   //  printf("----------------------------------------------------\n");
+   //  printf("\tGood Afternoon!\n");
+   //  printf("My name is : %s  \n", FullName);
+   //  printf("I am %d \n", age );
+   //  printf("And I am from %s\n",province);
+   //  printf("My major is %s\n", major);
+   //  printf("----------------------------------------------------\n");
+    // char fullName[20] = "Gech";
+    // int age =18;
+    // char major[30] = "C Programming";
+    // int year = 2024;
+    // char university[20] = "TGI Institute";
+    //
+    // printf("\n\t Display the Information of student\n");
+    // printf("Full Name: %s \n",fullName);
+    // printf("Age is: %d  \n",age);
+    // printf("Major is :%s \n",major);
+    // printf("Year of study: %d \n",year);
+    // printf("University is :%s \n", university);
+
+    // Create the program to execute the operation number:
+    int x = 25;
+    int y = 6;
+    // Addition operator
+    int addtion = x + y;
+    printf("Addition of X+Y is: %d\n",addtion);
+    // Substraction
+    int substraction = x -y;
+    printf("Substaction of X-Y is: %d\n",substraction);
+    //Multiplication
+    int mulitplication = x*y;
+    printf("Multiplication of X*Y is:%d\n",mulitplication);
+    //Division
+    int division =(float) x /y;
+    printf("Division of X/Y is:%d",division);
+    int remainder = x%y;
+    printf("Remainder is %d",remainder);
+
+
+
+    return 0;
+}
